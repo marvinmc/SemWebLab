ec6d2110df1fe95149c1f07cbd16ce08929f2d0b
marvinmc@kth.se <marvinmc@kth.se>
2017-11-06 20:16:36 +0100

    update

--- semanticwebAssign1/src/main/java/at/tuwien/geoinfo/dione/semanticwebAssign1/Exercise2.java
+++ semanticwebAssign1/src/main/java/at/tuwien/geoinfo/dione/semanticwebAssign1/Exercise2.java
@@ -1,11 +1,48 @@
 package at.tuwien.geoinfo.dione.semanticwebAssign1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 
 public class Exercise2 
 {
 	public static void main(String[] args) 
 	{
+		try {
 		
+		Model temp = TUM.TUModel;	
+		
+		//Set Prefixes
+		//tum
+		String tumNS 		= "http://data.ifs.tuwien.ac.at/study.rdf/";
+		String tumNSprefix  = "tum";
+		//FOAF
+		String foafNS		= FOAF.NAMESPACE.getNameSpace();
+		String foafNSprefix	= "foaf";
+		//RDFS
+		String rdfsNS 		= RDFS.getURI();
+		String rdfsNSPrefix = "rdfs";
+		//RDF
+		String rdfNS		= RDF.getURI();
+		String rdfNSPrefix	= "rdf";
+		//set Prefixes
+		temp.setNsPrefix(foafNSprefix,foafNS);
+		temp.setNsPrefix(rdfsNSPrefix, rdfsNS);
+		temp.setNsPrefix(tumNSprefix, tumNS);
+		temp.setNsPrefix(rdfNSPrefix, rdfNS);
+		FileWriter out = new FileWriter( "C:/EigeneUebungen/EinfuehrungSemWeb/Assignment1/Ex1/"+"rdfsmodel.ttl" );
+		temp.write(out, "Turtle");
+		
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
